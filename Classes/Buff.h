d5be9da03418c28d091cd0d0f76ee20c02f1f24c
StolfdaInuit <786617604@qq.com>
2018-06-29 03:42:25 +0800

    feat: Buffs

--- Classes/Buff.h
+++ Classes/Buff.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Unit.h"
+#include "RWorld.h"
 class Unit;
 
 namespace Buffs {
@@ -17,13 +18,56 @@ class Buff
 protected:
 	bool effective;
 	Unit * host;
+	float effectiveTime;
+	int duration;
+	int effectiveValue;
 public:
-	Buff(Unit* host);
+	Buff(Unit* host, int duration);
 	virtual bool shouldRemove()=0;
 	virtual int remove()=0;
 	virtual int enEffect()=0;
 	virtual bool isEffective()=0;
 	virtual bool disEffect()=0;
+	int getTimeLeft() const;
+	float getEffectiveTime() const;
 	virtual ~Buff();
 };
 
+class AttackPowerBuff :
+	public Buff
+{
+public:
+	AttackPowerBuff();
+	~AttackPowerBuff();
+	bool shouldRemove();
+	int remove();
+	int enEffect();
+	bool isEffective();
+	bool disEffect();
+};
+
+class AttackSpeedBuff :
+	public Buff
+{
+public:
+	AttackSpeedBuff();
+	~AttackSpeedBuff();
+	bool shouldRemove();
+	int remove();
+	int enEffect();
+	bool isEffective();
+	bool disEffect();
+};
+
+class MovingSpeedBuff :
+	public Buff
+{
+public:
+	MovingSpeedBuff();
+	~MovingSpeedBuff();
+	bool shouldRemove();
+	int remove();
+	int enEffect();
+	bool isEffective();
+	bool disEffect();
+};
